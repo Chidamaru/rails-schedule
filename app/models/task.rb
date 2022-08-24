931bc156086115763d5aad1d21685c59da3fd7b9
下高原汐理 <shimo.shiori@kakougenshiorinoMacBook-Air.local>
2022-08-24 23:20:51 +0900

    開始終了日のバリデーションが不明

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,10 @@
 class Task < ApplicationRecord
+
+    validate :start_end_check
+
+    def start_end_check
+      self.finish < self.start
+      errors.add(:finish, "は開始日より前の日付は登録できません。") 
+    end
+  
 end
