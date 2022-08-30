31c072c25d9f8c52d4046b6fb630e95c206dbca8
下高原汐理 <shimo.shiori@kakougenshiorinoMacBook-Air.local>
2022-08-30 21:56:09 +0900

    日付のバリデーションを追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,11 +1,18 @@
 class Task < ApplicationRecord
 
     validate :start_end_check
+    validate :startday
 
     def start_end_check
       if self.finish < self.start
        errors.add(:finish, "は開始日より前の日付は登録できません。") 
       end
     end
-  
+
+    def startday
+      return if start.blank?
+      errors.add(:start, "は今日以降のものを選択してください") if start < Date.today
+    end
+
 end
+ 
